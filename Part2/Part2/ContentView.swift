cb2c38b5a7fe3d95e27f9e22a23f7ac3f181c361
Sylar <swlfigo@gmail.com>
2020-07-08 22:41:39 +0800

    Part2

---
+++ Part2/Part2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Part2
+//
+//  Created by Sylar on 2020/7/8.
+//  Copyright © 2020 Sylar. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
